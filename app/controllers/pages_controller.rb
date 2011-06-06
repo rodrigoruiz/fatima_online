9d2f6024d26bba8a35089c318019c01f18336ea9
Rodrigo Ruiz <rodrigo.ruiz7@gmail.com>
2011-06-06 15:29:44 -0400

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,26 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
-
+  
   def procedimentos
+    @title = "Procedimentos"
   end
-
+  
   def consultorios
+    @title = "Consultorios"
   end
-
+  
+  def convenios
+    @title = "Convenios"
+  end
+  
   def dra_fatima
+    @title = "Dra. Maria de Fatima Maklouf Amorim Ruiz"
   end
-
+  
   def contato
+    @title = "Contato"
   end
-
+  
 end
