23d8e46d24a8b275c7b02cea0e5cb2be8c097721
Rodrigo Ruiz <rodrigo.ruiz7@gmail.com>
2011-06-06 09:42:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FatimaOnline::Application.config.secret_token = '2440019b09954f2172fc9cd1c4f012ff34e57645f1289c257b9c839840d6ce228a023aaf1a600599cfcc77a96a9267c0b8f0e5a5785f365be9d5fd2f4f1d2394'
